9236709db48b5311dc30a32372d9785121226af2
hgjohn <103093070+hgjohn@users.noreply.github.com>
2022-10-07 16:29:05 -0700

    Add files via upload

---
+++ nutritionchecker.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = "https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
+
+querystring = {"ingr":"apple", "ingr":"banana"}
+
+headers = {
+	"X-RapidAPI-Key": "66c11ad3c1mshcab242ab4c9a6abp19ed96jsne88329d755ee",
+	"X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+ 
+nutrients = response.text.find("nutrients")
+end_nutrients = response.text.find('}')
+nutrient_dict = json.loads(response.text[nutrients+11:end_nutrients+1])
+print(nutrient_dict)
