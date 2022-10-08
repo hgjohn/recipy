4a1ac561ae8b19bc4f371904c431fb13494dbd09
hgjohn <103093070+hgjohn@users.noreply.github.com>
2022-10-07 19:31:38 -0700

    added themealdb api interfacing

--- nutritionchecker.py
+++ nutritionchecker.py
@@ -1,18 +1,37 @@
 import requests
 import json
 
-url = "https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
+def fetch_meal():
+#user input meal is used to get ingredients dict from themealdb api
+#output ingr ran through edamam api to get nutrition dict
+    print('Enter a meal')
+    mealname = input()
 
-querystring = {"ingr":"apple", "ingr":"banana"}
+    mealapi = 'https://www.themealdb.com/api/json/v1/1/search.php?s='+mealname
+    mealresponse = requests.request("GET", mealapi)
+    first_ingr = mealresponse.text.find("strIngredient1")
+    last_ingr = mealresponse.text.find("strMeasure1")
+    meal_dict = json.loads("{ "+mealresponse.text[first_ingr-1:last_ingr-2]+" }")
+    print(meal_dict)
 
-headers = {
-	"X-RapidAPI-Key": "66c11ad3c1mshcab242ab4c9a6abp19ed96jsne88329d755ee",
-	"X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com"
-}
 
-response = requests.request("GET", url, headers=headers, params=querystring)
- 
-nutrients = response.text.find("nutrients")
-end_nutrients = response.text.find('}')
-nutrient_dict = json.loads(response.text[nutrients+11:end_nutrients+1])
-print(nutrient_dict)
+    
+    url = "https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
+    querystring = {"ingr":"apple", "ingr":"banana"}
+    #meal_dict keys must all read "ingr" before querystring will accept it as input
+    headers = {
+	    "X-RapidAPI-Key": "66c11ad3c1mshcab242ab4c9a6abp19ed96jsne88329d755ee",
+	    "X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com"
+    }
+
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    nutrients = response.text.find("nutrients")
+    end_nutrients = response.text.find('}')
+    nutrient_dict = json.loads(response.text[nutrients+11:end_nutrients+1])
+
+    print(nutrient_dict)
+
+
+fetch_meal()
