efe007377cefc630f6d57a47074c54732caeb402
hungryantnuns <112605332+hungryantnuns@users.noreply.github.com>
2022-10-08 23:12:34 -0700

    Add files via upload

---
+++ reciPy.py
@@ -0,0 +1,39 @@
+# reciPy.py
+
+import PySimpleGUI as sg
+
+# sg.theme('')
+
+# ----- child components
+recipe_searcher_column = [
+    [sg.InputText(), sg.Button('Search')],
+    [sg.Button("OK")]
+]  
+
+recipe_browser_column = [
+    [sg.InputText(), sg.Button('Search')],
+    [sg.Button("OK")]
+]  
+
+# ----- parent component -----
+layout = [
+    [
+        sg.Column(recipe_searcher_column),
+        sg.VSeparator(),
+        sg.Column(recipe_browser_column)
+    ]
+]  
+                     
+
+# Create the window
+window = sg.Window("Demo", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
