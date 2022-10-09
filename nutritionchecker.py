54365747f45d42ed314cd7d73bde0e137a57db53
hgjohn <103093070+hgjohn@users.noreply.github.com>
2022-10-09 12:38:46 -0700

    Add files via upload

--- nutritionchecker.py
+++ nutritionchecker.py
@@ -42,16 +42,17 @@ def fetch_meal():
             meal_iter += 1
     #input an available recipe
     
-    print(recipe_list)
+    
     recipe_layout = [  [sg.Text('Input recipe selection')],
             [
         sg.Listbox(
-            values = recipe_list, enable_events=True, size=(40, 20), key="-INGREDIENT LIST-"
+            values = recipe_list, enable_events=True, size=(40, 20), key="-RECIPE LIST-"
         )
     ],
             [sg.InputText()],
             [sg.Button('Ok'), sg.Button('Cancel')]]
 
+    
     def recipe_select():
         window = sg.Window('Recipe Select', recipe_layout)
         while True:
@@ -59,6 +60,7 @@ def fetch_meal():
             if event == sg.WIN_CLOSED or event == 'Cancel':
                 break
             if event == 'Ok':
+                global recipe
                 recipe = values[0]
                 print('you entered ', recipe)
                 window.close()
@@ -66,15 +68,13 @@ def fetch_meal():
     recipe_select()
 
     
-
-    recipe_sel = recipe
     
     #gets instructions, ingr, and measurements for recipe
     recipe_iter = 0
     while recipe_iter != len(meal_dict['meals']):
         if recipe_iter == len(meal_dict['meals']):
             break
-        elif meal_dict['meals'][recipe_iter]['strMeal'] == recipe_sel:
+        elif meal_dict['meals'][recipe_iter]['strMeal'] == recipe:
             recipe_dict = meal_dict['meals'][recipe_iter]
             recipe_iter += 1
         else:
@@ -84,9 +84,17 @@ def fetch_meal():
     ingr_list = [recipe_dict['strIngredient1'], recipe_dict['strIngredient2'], recipe_dict['strIngredient3'], recipe_dict['strIngredient4'], recipe_dict['strIngredient5'], recipe_dict['strIngredient6'], recipe_dict['strIngredient7'], recipe_dict['strIngredient8'], recipe_dict['strIngredient9'], recipe_dict['strIngredient10'], recipe_dict['strIngredient11'], recipe_dict['strIngredient12'], recipe_dict['strIngredient13'], recipe_dict['strIngredient14'], recipe_dict['strIngredient15'], recipe_dict['strIngredient16'], recipe_dict['strIngredient17'], recipe_dict['strIngredient18'], recipe_dict['strIngredient19'], recipe_dict['strIngredient20'], ]
     meas_list = [recipe_dict['strMeasure1'], recipe_dict['strMeasure2'], recipe_dict['strMeasure3'], recipe_dict['strMeasure4'], recipe_dict['strMeasure5'], recipe_dict['strMeasure6'], recipe_dict['strMeasure7'], recipe_dict['strMeasure8'], recipe_dict['strMeasure9'], recipe_dict['strMeasure10'], recipe_dict['strMeasure11'], recipe_dict['strMeasure12'], recipe_dict['strMeasure13'], recipe_dict['strMeasure14'], recipe_dict['strMeasure15'], recipe_dict['strMeasure16'], recipe_dict['strMeasure17'], recipe_dict['strMeasure18'], recipe_dict['strMeasure19'], recipe_dict['strMeasure20'], ]
     
+
+
+
     #this empties null values from ingr_list
     ingr_list = list(filter(None, ingr_list))
     ingr_keys = list()
+
+
+    c = []
+    for x, y in zip(ingr_list, meas_list):
+        c += [x, y]
 
     #this creates a list with 'ingr' as each value equal in length to the ingr_list
     ingr_info = [ingr_list, ingr_keys]
@@ -98,6 +106,23 @@ def fetch_meal():
         array += ['ingr'] * (max_length - len(array))
 
     ingr_query = dict(zip(ingr_keys, ingr_list))
+
+    final_layout = [  [sg.Text('Input recipe selection')],
+            [sg.Text('Ingredients')],
+    [sg.Multiline(c, size=(45, 5), expand_x=False, expand_y=True, key='-INGREDIENTMLINE-')],
+    [sg.Text('Directions')],
+    [sg.Multiline(recipe_inst, size=(45, 5), expand_x=False, expand_y=True, key='-DIRECTIONMLINE-')]
+    ]
+    
+    def final_boss():
+        window = sg.Window('Recipe info', final_layout)
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event == 'Cancel':
+                break
+        
+
+    final_boss()
 
     
     #api call for nutrition based on ingr dict
@@ -118,11 +143,15 @@ def fetch_meal():
 
     nutrient_code = {'FIBTG': 'Fiber', 'CHOCDF': 'Carbohydrates', 'ENERC_KCAL': 'Kilocalories', 'FAT': 'Total fat', 'PROCNT': 'Protein'}
 
-    print(ingr_query)
-    print(ingr_list)
-    print(meas_list)
-    print(recipe_inst)
-    print(nutrient_dict)
+    
+    
+    #print(ingr_query)
+    #print(ingr_list)
+    #print(meas_list)
+    #print(recipe_inst)
+    #print(nutrient_dict)
+
+    
 
 
 fetch_meal()
