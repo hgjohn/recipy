855136e508903b8d4950754d118fb83586a0028f
hungryantnuns <hzhu8127@gmail.com>
2022-10-21 14:59:14 -0700

    Try #2

--- reciPy.py
+++ reciPy.py
@@ -1,37 +1,92 @@
+# reciPy.py
+
 import PySimpleGUI as sg
 import requests
 import json
 
-def fetch(mealname):
-    if(not mealname):
-        return None
+def list_meal(mealname):
+# generates recipe info from selected input
+# outputs ingredients, measurements, nutrition, and instructions to display
 
-    api = 'https://www.themealdb.com/api/json/v1/1/search.php?s='+mealname
-    response = requests.request("GET", api)
-    meal_dict = json.loads(response.text)  
-    return meal_dict['meals']
+    mealapi = 'https://www.themealdb.com/api/json/v1/1/search.php?s='+mealname
+    mealresponse = requests.request("GET", mealapi)
+    meal_dict = json.loads(mealresponse.text)
+    #gets meal names
+    meal_iter = 0
+    recipe_list = []
+    while meal_iter != len(meal_dict['meals']):
+        if meal_iter == len(meal_dict['meals']):
+            break
+        else:
+            recipe_list.append(meal_dict['meals'][meal_iter]['strMeal'])
+            meal_iter += 1
+    return recipe_list
 
-def list_meal(func):
+#list_meal('chicken')
 
-    if(not func):
-        return None
-    result = []
-    for i in func:
-        result.append(i['strMeal'])
-    return result
+def get_mealingr(mealname):
 
-def ingredients(func):
-    result = []
-    counter = 1
-    for i in func[0]:
-        if(i == 'strIngredient' + str(counter) and func[0][i]): 
-            result.append(func[0][i] + ": " + func[0]['strMeasure' + str(counter)])
-            counter += 1
-    return result
+    mealapi = 'https://www.themealdb.com/api/json/v1/1/search.php?s='+mealname
+    mealresponse = requests.request("GET", mealapi)
+    meal_dict = json.loads(mealresponse.text)
+    #input an available recipe
+    
+    #gets instructions, ingr, and measurements for recipe
+    recipe_iter = 0
+    while recipe_iter != len(meal_dict['meals']):
+        if recipe_iter == len(meal_dict['meals']):
+            break
+        elif meal_dict['meals'][recipe_iter]['strMeal'] == mealname:
+            recipe_dict = meal_dict['meals'][recipe_iter]
+            recipe_iter += 1
+        else:
+            recipe_iter += 1
+
+    recipe_inst = recipe_dict['strInstructions']
+    ingr_list = [recipe_dict['strIngredient1'], recipe_dict['strIngredient2'], recipe_dict['strIngredient3'], recipe_dict['strIngredient4'], recipe_dict['strIngredient5'], recipe_dict['strIngredient6'], recipe_dict['strIngredient7'], recipe_dict['strIngredient8'], recipe_dict['strIngredient9'], recipe_dict['strIngredient10'], recipe_dict['strIngredient11'], recipe_dict['strIngredient12'], recipe_dict['strIngredient13'], recipe_dict['strIngredient14'], recipe_dict['strIngredient15'], recipe_dict['strIngredient16'], recipe_dict['strIngredient17'], recipe_dict['strIngredient18'], recipe_dict['strIngredient19'], recipe_dict['strIngredient20'], ]
+    meas_list = [recipe_dict['strMeasure1'], recipe_dict['strMeasure2'], recipe_dict['strMeasure3'], recipe_dict['strMeasure4'], recipe_dict['strMeasure5'], recipe_dict['strMeasure6'], recipe_dict['strMeasure7'], recipe_dict['strMeasure8'], recipe_dict['strMeasure9'], recipe_dict['strMeasure10'], recipe_dict['strMeasure11'], recipe_dict['strMeasure12'], recipe_dict['strMeasure13'], recipe_dict['strMeasure14'], recipe_dict['strMeasure15'], recipe_dict['strMeasure16'], recipe_dict['strMeasure17'], recipe_dict['strMeasure18'], recipe_dict['strMeasure19'], recipe_dict['strMeasure20'], ]
+    
+    #this empties null values from ingr_list
+    ingr_list = list(filter(None, ingr_list))
+    c = []
+    for x, y in zip(ingr_list, meas_list):
+        c.append(x + ' ' + y)
+    return c
+    #return ingr_list
+    #print(ingr_list)   
+    
+
+def get_mealdir(mealname):
+
+    mealapi = 'https://www.themealdb.com/api/json/v1/1/search.php?s='+mealname
+    mealresponse = requests.request("GET", mealapi)
+    meal_dict = json.loads(mealresponse.text)
+    #input an available recipe
+    
+    #gets instructions, ingr, and measurements for recipe
+    recipe_iter = 0
+    while recipe_iter != len(meal_dict['meals']):
+        if recipe_iter == len(meal_dict['meals']):
+            break
+        elif meal_dict['meals'][recipe_iter]['strMeal'] == mealname:
+            recipe_dict = meal_dict['meals'][recipe_iter]
+            recipe_iter += 1
+        else:
+            recipe_iter += 1
+    recipe_inst = recipe_dict['strInstructions']
+    return recipe_inst
 
 
+#get_mealingr('Chicken Handi')
 
-# ----- child -----
+
+# Changes the theme/color 
+# It's a place holder for now, until I figure out the values
+# sg.theme('')
+
+# ----- child components
+# meal names will be given to values as an array. Key can be referenced in the event loop.
+
 recipe_searcher_column = [
     [
         sg.InputText(size=(25), enable_events=True, key="-MEAL NAME-"), 
@@ -42,10 +97,11 @@ recipe_searcher_column = [
             values = [], enable_events=True, size=(30, 20), key="-RECIPE LIST-"
         )
     ],
-    [sg.Button("Exit")]
+    [sg.Button("OK")]
 ]  
 
-# Displays meal information
+#Displays meal information
+#We have the option of displaying a picture, if we grab that link
 recipe_viewer_column = [
     [sg.Text('Ingredients')],
     [
@@ -58,7 +114,7 @@ recipe_viewer_column = [
     
 ]  
 
-# ----- parent -----
+# ----- parent component -----
 layout = [
     [
         sg.Column(recipe_searcher_column),
@@ -66,6 +122,7 @@ layout = [
         sg.Column(recipe_viewer_column)
     ]
 ]  
+                     
 
 # Create the window
 window = sg.Window("Recipe Browser", layout)
@@ -75,22 +132,17 @@ while True:
     event, values = window.read()
     # End program if user closes window or
     # presses the OK button
-    if event == "Exit" or event == sg.WIN_CLOSED:
+    if event == "OK" or event == sg.WIN_CLOSED:
         break
     # This is the part of the code that needs to access Recipe names
     # For example, .update(mealName())
     if event == "Search":
-        mealname = values["-MEAL NAME-"]
-        recipelist = list_meal(fetch(mealname))
-        if(not mealname or not recipelist): # A blank, type != string, or otherwise bad input was given
-            sg.popup("No results", keep_on_top=False)
-        else:
-            window["-RECIPE LIST-"].update(recipelist)
-    if event == "-RECIPE LIST-" and values["-RECIPE LIST-"]: # A recipe was chosen from the listbox
+        mealName = values["-MEAL NAME-"]
+        window["-RECIPE LIST-"].update(list_meal(mealName))
+    if event == "-RECIPE LIST-": # A file was chosen  from the listbox
         recipename = ''.join(values["-RECIPE LIST-"])
-        window["-INGREDIENT LIST-"].update(ingredients(fetch(recipename)))
-        window["-DIRECTIONMLINE-"].update(fetch(recipename)[0]['strInstructions'])
+        window["-INGREDIENT LIST-"].update(get_mealingr(recipename))
+        window["-DIRECTIONMLINE-"].update(get_mealdir(recipename))
 
 
 window.close()
-
